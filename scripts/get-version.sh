2e8b8be69dc87d9931a4b24ab066eb0c09ef3e2b
erri120 <erri120@protonmail.com>
2023-01-21 17:53:45 +0100

    Update script

--- scripts/get-version.sh
+++ scripts/get-version.sh
@@ -6,8 +6,8 @@ LATEST_VERSION=$(
     grep -E -oh '[0-9](\.[0-9](\.[0-9])?)?$'
 )
 
+echo "Latest version: $LATEST_VERSION"
+
 if [[ ! -z $GITHUB_OUTPUT ]]; then
     echo "LATEST_VERSION=$LATEST_VERSION" >> $GITHUB_OUTPUT
-else
-
-echo "Latest version: $LATEST_VERSION"
+fi
